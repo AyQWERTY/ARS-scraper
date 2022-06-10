668d113aefd45089596375ab782f104c36b075ad
Andrew <andreygti728@gmail.com>
2022-06-10 22:55:01 +0300

    Added writing to and reading from a file

--- backend/index.js
+++ backend/index.js
@@ -4,16 +4,16 @@ const fs = require("fs");
     
 const app = express();
 const jsonParser = express.json();
-const filePath = "prices.json";
+const filePath = "./backend/prices.json";
 
 
 app.get("/refresh", function(req, res) {
     var stores = {
         eneba: [],
         g2a: [],
-        plati: [],
-        wmcentre: [],
-        ggsel: [],
+        // plati: [],
+        // wmcentre: [],
+        // ggsel: [],
     };
     
     Promise.all([parsers.eneba, parsers.g2a]).then(values => { // not sure it works more than once
@@ -21,14 +21,14 @@ app.get("/refresh", function(req, res) {
         stores.g2a = values[1];
 
         console.log(`New Request! Answered stats: Eneba: ${stores.eneba.length} | G2A: ${stores.g2a.length}`)
-        res.send(stores); // Temporarily. Until the address "/prices" is implemented.
+        var json = JSON.stringify(stores);
+        fs.writeFileSync(filePath, json, 'utf-8');
+        res.send('Слава Україні!')
     });
-
-    // res.send('Слава Україні!')
 });
 
 app.get("/prices", function(req, res){
-    const content = fs.readFileSync(filePath,"utf8");
+    const content = fs.readFileSync(filePath,"utf-8");
     const prices = JSON.parse(content);
     res.send(prices);
 });
